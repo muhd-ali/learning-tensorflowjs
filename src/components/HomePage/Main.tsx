991680a1e84377fc22e5ec52feeb42e25fba3d5e
Muhammad Ali <this.is.muhammad.ali@gmail.com>
2019-05-14 17:12:20 -0500

    added github link to welcome page

--- src/components/HomePage/Main.tsx
+++ src/components/HomePage/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withStyles, WithStyles, Theme, Typography, Divider, ListItem, List } from '@material-ui/core';
+import { withStyles, WithStyles, Theme, Typography, Divider, ListItem, List, Grid } from '@material-ui/core';
 import PaperTemplate from 'components/Templates/Paper/Main';
 
 const styles = (theme: Theme) => ({
@@ -17,36 +17,74 @@ class Main extends React.Component<MainProps, MainState> {
         let { theme } = this.props;
         return (
             <PaperTemplate theme={theme}>
-                <Typography variant='h4'>
-                    Welcome
-                </Typography>
-                <br />
-                <Typography variant='body1'>
-                    The project is a react app that I made to play around with tensorflow.js.
-                </Typography>
-                <Divider />
-                <br />
-                <br />
-                <Typography variant='h6'>
-                    Features
-                </Typography>
-                <List>
-                    <ListItem>
+                <Grid
+                    container
+                    spacing={24}
+                >
+                    <Grid
+                        item
+                        xs={12}
+                    >
+                        <Typography variant='h4'>
+                            Welcome
+                        </Typography>
+                        <br />
                         <Typography variant='body1'>
-                            Using <a href='http://material-ui.com'>Material-UI</a> for GUI.
-                </Typography>
-                    </ListItem>
-                    <ListItem>
+                            The project is a react app that I made to play around with tensorflow.js.
+                        </Typography>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                    >
+                        <Divider />
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                    >
+                        <Typography variant='h6'>
+                            Features
+                        </Typography>
+                        <List>
+                            <ListItem>
+                                <Typography variant='body1'>
+                                    Using <a href='http://material-ui.com'>Material-UI</a> for GUI.
+                                </Typography>
+                            </ListItem>
+                            <ListItem>
+                                <Typography variant='body1'>
+                                    Using <a href='https://github.com/ReactiveX/rxjs'>RxJS</a> for putting events on variable changes.
+                                </Typography>
+                            </ListItem>
+                            <ListItem>
+                                <Typography variant='body1'>
+                                    Using <a href='https://github.com/tensorflow/tfjs-models/tree/master/coco-ssd'>COCO-SSD</a> model for object detection.
+                                </Typography>
+                            </ListItem>
+                        </List>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                    >
+                        <Divider />
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                    >
+                        <Typography variant='h6'>
+                            Github Repo
+                        </Typography>
+                        <br/>
                         <Typography variant='body1'>
-                            Using <a href='https://github.com/ReactiveX/rxjs'>RxJS</a> for putting events on variable changes.
-                </Typography>
-                    </ListItem>
-                    <ListItem>
-                        <Typography variant='body1'>
-                            Using <a href='https://github.com/tensorflow/tfjs-models/tree/master/coco-ssd'>COCO-SSD</a> model for object detection.
-                </Typography>
-                    </ListItem>
-                </List>
+                            <a href='https://github.com/muhd-ali/learning-tensorflowjs'>
+                                https://github.com/muhd-ali/learning-tensorflowjs
+                            </a>
+                        </Typography>
+                    </Grid>
+                </Grid>
             </PaperTemplate>
         )
     }
